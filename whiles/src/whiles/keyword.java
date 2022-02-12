c2129a0753cf92bbc94a59e66cddbeb408ed0224
Akash <sableakash20@gmail.com>
2022-02-12 12:04:31 +0530

    first commit

---
+++ whiles/src/whiles/keyword.java
@@ -0,0 +1,20 @@
+package whiles;
+
+
+	class car
+	{
+		final int max_speed=120;
+		void controlSpeed()
+		{
+		//	max_speed=180; This will generate an error you Can't change value of final stated variables.
+		System.out.println("Max Speed value is " +max_speed);
+		}
+	}
+	public class keyword {
+
+		public static void main(String[] args) {
+			car obj=new car();
+			obj.controlSpeed();
+	}
+
+}
---
+++ whiles/src/whiles/scanners.java
@@ -0,0 +1,18 @@
+package whiles;
+import java.util.*;
+
+public class scanners {
+
+
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			System.out.println("Sum is"+(a+b));
+			scanners obj=new scanners();
+			System.out.println(obj.getClass());
+			System.out.println(obj.hashCode());
+			
+		}
+
+	}
